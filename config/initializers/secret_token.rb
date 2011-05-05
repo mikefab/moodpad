0d298d2834a5b6a991c0f78c82ab31b2daaa41c2
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-05-05 15:41:08 -0300

    adding all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pad::Application.config.secret_token = '193f4d2749aa5e6293c36a3b93f39166d4952bc69a0bd8cc1f329359b143acae3e77f55948e0f0122d3650babc18f728220cef6543d16fc3d17877ff2d0f6f02'
